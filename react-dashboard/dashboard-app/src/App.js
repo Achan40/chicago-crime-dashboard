3fccee1a0b9b4c897e9bf47dd95d1e6f8de9502f
Aaron <chanman2841@gmail.com>
2021-12-21 22:47:54 -0600

    create working chart!

--- react-dashboard/dashboard-app/src/App.js
+++ react-dashboard/dashboard-app/src/App.js
@@ -1,21 +1,45 @@
-function App() {
+import cubejs from "@cubejs-client/core";
+import { QueryRenderer } from "@cubejs-client/react";
+import { BarChart, Bar, XAxis, YAxis, Tooltip } from "recharts";
+
+const cubejsApi = cubejs(
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NDAxNDYyODMsImV4cCI6MTY0MDIzMjY4M30.zsUoQ48_CIY8os1KvfWYcaBBkS_YzxZ3RsgH5YV5GIc',
+  { apiUrl: 'http://localhost:4000/cubejs-api/v1' }
+);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryRenderer
+      query={{
+        "measures": [
+          "Corecrimedata.count"
+        ],
+        "timeDimensions": [],
+        "order": {
+          "Corecrimedata.count": "desc"
+        },
+        "filters": [],
+        "dimensions": [
+          "Corecrimedata.year"
+        ],
+        "limit": 5000
+      }}
+      cubejsApi={cubejsApi}
+      render={({ resultSet }) => {
+        if (!resultSet) {
+          return "Loading Analytics...";
+        }
+        return (
+          <BarChart width={600} height={300} data={resultSet.rawData()}>
+            <XAxis dataKey="Corecrimedata.year" stroke="#8884d8" />
+            <YAxis />
+            <Tooltip />
+            <Bar barSize={30} dataKey="Corecrimedata.count" stroke="#8884d8" />
+          </BarChart>
+        );
+      }}
+    />
   );
-}
+};
 
 export default App;
