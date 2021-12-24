2033da02a247377562678ac4a7d42d9969de4bf3
Aaron <chanman2841@gmail.com>
2021-12-24 16:53:36 -0600

    finish with database/table creation, loading bulk data, loading new data scripts

--- dbscripts/CrimeData.py
+++ dbscripts/CrimeData.py
@@ -1,8 +1,6 @@
-from mysql.connector import connection
 import requests
 import json
 import pandas as pd
-import mysql.connector
 from mysql.connector import connect, Error
 
 # local files containing credentials
@@ -28,7 +26,7 @@ class CrimeData:
     def create_database(self, dbname):
         self.cur.execute("CREATE DATABASE " + dbname)
 
-    # create the corecrime data table
+    # create the corecrimedata table
     def create_corecrimedata(self):
         create_corecrime_table_query = """
         CREATE TABLE corecrimedata (
@@ -57,7 +55,7 @@ class CrimeData:
         self.cur.execute(create_corecrime_table_query)
         self.con.commit()
 
-    # use get_bulk_crime_data or ____to get the df to pass into this method
+        # use get_bulk_crime_data or ____to get the df to pass into this method
     def insert_corecrimedata(self, df):
         insert_corecrime = """
         INSERT INTO corecrimedata (
@@ -132,15 +130,50 @@ class CrimeData:
                 )
             try:
                 self.cur.execute(insert_corecrime,data)
+                if row.Index % 100 == 0:
+                    print(row.Index)
             except:
                 print(row)
                 pass
         self.con.commit()
 
-    # close the connection, should always be done when we are finished with the object
-    def close(self):
-        self.con.close()
-        self.cur.close()
+    # create the commareas table (no api for this dataset, must upload via csv)
+    # commareas describest the community areas of chicago, generally a static dataset
+    def create_commareas(self):
+        create_commareas_table_query = """
+        CREATE TABLE commareas (
+            community_area INT,
+            community_desc TEXT
+        )
+        """
+        self.cur.execute(create_commareas_table_query)
+        self.con.commit()
+
+    # load the csv file from https://data.cityofchicago.org/api/views/igwz-8jzy/rows.csv?accessType=DOWNLOAD&bom=true&format=true into a dataframe and use it as an input
+    def insert_commareas(self,df):
+        insert_commareas = """
+        INSERT INTO commareas (
+            community_area,
+            community_desc
+        )
+        VALUES (%s,%s)
+        ON DUPLICATE KEY UPDATE
+            community_area=VALUES(community_area),
+            community_desc=VALUES(community_desc)
+        """
+        for row in df.itertuples():
+            data = (
+                row.AREA_NUMBE,
+                row.COMMUNITY,
+                )
+            try:
+                self.cur.execute(insert_commareas,data)
+            except:
+                print(row)
+                pass
+        self.con.commit()
+
+    
 
     # Retreive data from Socrata API
     def get_bulk_crime_data(self, startyear, endyear, limit='1000000'):
@@ -154,13 +187,7 @@ class CrimeData:
         # Load data into a pandas dataframe
         data = pd.DataFrame(df)
 
-        # Some typecasting
-        # data['date'] = pd.to_datetime(data['date'], errors='coerce')
-        # data['updated_on'] = pd.to_datetime(data['updated_on'], errors='coerce')
-        # data['year'] = pd.to_datetime(data['year'], errors='coerce')
-        data['community_area'] = pd.to_numeric(data['community_area'], downcast='integer',errors='coerce')
-
-        # Location is dropped since each row is a dictionary, mysql db doesn't like this
+        # Drop location column since each entry is a dictionary, mysql db doesn't like this datatype
         # also rare edge case if there is no "location" column, json format field won't appear if nan
         try:
             data.drop(columns=['location'], inplace=True)
@@ -171,3 +198,35 @@ class CrimeData:
         data = data.where(pd.notnull(data), None)
 
         return data
+
+    # Get most recent updated_on entry from database
+    def most_recent_update_on_corecrimedata(self):
+        update_on_query = "SELECT MAX(updated_on) FROM corecrimedata"
+        result = None
+        try:
+            self.cur.execute(update_on_query)
+            result = self.cur.fetchall()
+        except Exception as e:
+            print(e)
+        # returns the a datetime object, y-m-d
+        return result[0][0].date()
+
+
+    # Get updated data from Socrata API
+    def get_corecrimedata_updates(self,timestamp,limit):
+        # String literal, pull all available data from API for some time frame
+        url = f'https://data.cityofchicago.org/resource/ijzp-q8t2.json?$where=updated_on >= \'{timestamp}\' &$order=date ASC&$limit={limit}'
+        # Using our app token
+        headers = {'Accept': 'application/json', 'X-App-Token': APP_TOKEN}
+        resp = requests.get(url,headers=headers)
+        df = json.loads(resp.text)
+
+        # Load data into a pandas dataframe
+        data = pd.DataFrame(df)
+
+        return data
+
+    # close the connection, should always be done when we are finished with the object
+    def close(self):
+        self.con.close()
+        self.cur.close()
--- dbscripts/main.py
+++ dbscripts/main.py
@@ -1,11 +1,27 @@
-from os import pardir
+from os import pardir, read
 import pandas as pd
 from CrimeData import CrimeData
 
 if __name__ == '__main__':
     cdobj = CrimeData()
+
+    # create corecrimedata table, commareas table
+    # cdobj.create_corecrimedata()
+    # cdobj.create_commareas()
+
+    # populate commareas table
+    # commareas = pd.read_csv('./datasets/CommAreas.csv')
+    # cdobj.insert_commareas(commareas)
+
+    # loading bulk data into corecrimedatatable
     df = cdobj.get_bulk_crime_data('2010','2011','1000000')
     cdobj.insert_corecrimedata(df)
+
+    # retrieving and sending updates
+    # latest = cdobj.most_recent_update_on_corecrimedata()
+    # updates = cdobj.get_corecrimedata_updates(timestamp=latest,limit='50000')
+    # cdobj.insert_corecrimedata(updates)
+
     cdobj.close()
 
     
