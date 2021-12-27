0de5e25df93df693470894bd10173bedf1c2ccf6
Aaron <chanman2841@gmail.com>
2021-12-26 22:31:18 -0600

    create first pre-aggregation, dropdown menu from query with callbacks

--- react-dashboard/dashboard-app/src/CommDropDown.js
+++ react-dashboard/dashboard-app/src/CommDropDown.js
@@ -8,42 +8,65 @@ import 'bootstrap/dist/css/bootstrap.css';
 class CommDropDown extends Component {
     constructor() {
         super()
+        // link to cubejs
         this.cubejsApi = cubejs(
             'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NDAxNDYyODMsImV4cCI6MTY0MDIzMjY4M30.zsUoQ48_CIY8os1KvfWYcaBBkS_YzxZ3RsgH5YV5GIc',
             { apiUrl: 'http://localhost:4000/cubejs-api/v1' }
         );
+
+        // saving state
+        this.state = {
+            commarea: "Select a community area"
+        }
+
+        this.commareadescarr = []
+
+        // bind custom methods
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    // update the selected commarea when one is selected from the dropdown
+    // on the onClick event (once an item from the dropdown is selected), the state is set to the id of the selected dropdown item.
+    handleChange(event) {
+        this.setState({
+            commarea: event.target.id
+        })
     }
 
     render () {
         return (
-            <div>
-                <QueryRenderer
-                    query={{
-                        "measures": [
-                            "Corecrimedata.count"
-                          ],
-                        "dimensions": [
-                            "Commareas.communityDesc"
-                          ],
-                    }}
-                    cubejsApi={this.cubejsApi}
-                    render={({ resultSet }) => {
-                    if (!resultSet) {
-                        return "Loading Analytics...";
-                    }
-                    return (
-                        <div>tmp
-                            {console.log(resultSet.loadResponses["0"])}
-                        </div>
-                        );
-                    }}
-                />
-                <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                </DropdownButton>
-            </div>
+            <QueryRenderer
+                query={{
+                    "measures": [
+                        "Corecrimedata.count"
+                        ],
+                    "dimensions": [
+                        "Commareas.communityDesc"
+                        ],
+                    "order": {
+                        "Commareas.communityDesc": "asc"
+                        },
+                }}
+                cubejsApi={this.cubejsApi}
+                render={({ resultSet }) => {
+                if (!resultSet) {
+                    return "Loading Analytics...";
+                } else {
+                    // set variable for the community area description object
+                    this.commareadescarr = resultSet.loadResponses["0"].data;
+                }
+                return (
+                    <div>
+                        <DropdownButton id="dropdown-basic-button" title={this.state.commarea}>
+                            {/* genereate JSX in a loop */}
+                            {this.commareadescarr.map(item => (
+                                <Dropdown.Item onClick={this.handleChange} id ={item['Commareas.communityDesc']}>{item['Commareas.communityDesc']}</Dropdown.Item>
+                            ))}
+                        </DropdownButton>
+                    </div>
+                    );
+                }}
+            />
         )
     }
 }
--- react-dashboard/schema/Corecrimedata.js
+++ react-dashboard/schema/Corecrimedata.js
@@ -4,6 +4,10 @@ cube(`Corecrimedata`, {
   preAggregations: {
     // Pre-Aggregations definitions go here
     // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started  
+    countByCommunityDesc: {
+      dimensions: [Commareas.communityDesc],
+      measures: [Corecrimedata.count]
+    }
   },
   
   joins: {
