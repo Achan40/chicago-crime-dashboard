a7fb45fd842e988abe575dc57154420ebe71a568
Aaron <chanman2841@gmail.com>
2021-12-31 00:24:55 -0600

    Clean up datatable component

--- react-dashboard/dashboard-app/src/RawDataTable.js
+++ react-dashboard/dashboard-app/src/RawDataTable.js
@@ -1,81 +1,10 @@
-// import cubejs from '@cubejs-client/core';
-// import { QueryRenderer } from '@cubejs-client/react';
-// import { Spin } from 'antd';
-// import 'antd/dist/antd.css';
-// import React, { Component } from 'react';
-// import { Row, Col, Statistic, Table } from 'antd';
-// import { useDeepCompareMemo } from 'use-deep-compare'
-
 import cubejs from '@cubejs-client/core';
 import { QueryRenderer } from '@cubejs-client/react';
 import { Spin } from 'antd';
 import 'antd/dist/antd.css';
 import React, { Component } from 'react';
-import { Table } from 'antd';
-import { useDeepCompareMemo } from 'use-deep-compare';
 
-
-const formatTableData = (columns, data) => {
-  function flatten(columns = []) {
-    return columns.reduce((memo, column) => {
-      if (column.children) {
-        return [...memo, ...flatten(column.children)];
-      }
-
-      return [...memo, column];
-    }, []);
-  }
-
-  const typeByIndex = flatten(columns).reduce((memo, column) => {
-    return { ...memo, [column.dataIndex]: column };
-  }, {});
-
-  function formatValue(value, { type, format } = {}) {
-    if (value == undefined) {
-      return value;
-    }
-
-    if (type === 'boolean') {
-      if (typeof value === 'boolean') {
-        return value.toString();
-      } else if (typeof value === 'number') {
-        return Boolean(value).toString();
-      }
-
-      return value;
-    }
-
-    if (type === 'number' && format === 'percent') {
-      return [parseFloat(value).toFixed(2), '%'].join('');
-    }
-
-    return value.toString();
-  }
-
-  function format(row) {
-    return Object.fromEntries(
-      Object.entries(row).map(([dataIndex, value]) => {
-        return [dataIndex, formatValue(value, typeByIndex[dataIndex])];
-      })
-    );
-  }
-
-  return data.map(format);
-};
-
-const TableRenderer = ({ resultSet, pivotConfig }) => {
-    const [tableColumns, dataSource] = useDeepCompareMemo(() => {
-      const columns = resultSet.tableColumns(pivotConfig);
-      return [
-        columns,
-        formatTableData(columns, resultSet.tablePivot(pivotConfig)),
-      ];
-    }, [resultSet, pivotConfig]);
-    return (
-      <Table pagination={false} columns={tableColumns} dataSource={dataSource} />
-    );
-  };
-  
+import TableRenderer from './helpercomponents/TableRenderer';
 
 class RawDataTable extends Component {
     constructor() {
@@ -134,7 +63,7 @@ class RawDataTable extends Component {
               "Commareas.communityDesc"
               ],
               "filters": this.props.filters,
-              "limit": 100
+              "limit": 50
           }}
                   cubejsApi={this.cubejsApi}
                   resetResultSetOnChange={false}
@@ -160,7 +89,7 @@ class RawDataTable extends Component {
               "y": [],
               "fillMissingDates": true,
               "joinDateRange": false,
-              "limit": 100
+              "limit": 50
           }
                   })}
               />
@@ -170,169 +99,3 @@ class RawDataTable extends Component {
 }
 
 export default RawDataTable
-      
-// const formatTableData = (columns, data) => {
-//     function flatten(columns = []) {
-//       return columns.reduce((memo, column) => {
-//         if (column.children) {
-//           return [...memo, ...flatten(column.children)];
-//         }
-  
-//         return [...memo, column];
-//       }, []);
-//     }
-  
-//     const typeByIndex = flatten(columns).reduce((memo, column) => {
-//       return { ...memo, [column.dataIndex]: column };
-//     }, {});
-  
-//     function formatValue(value, { type, format } = {}) {
-//       if (value === undefined) {
-//         return value;
-//       }
-  
-//       if (type === 'boolean') {
-//         if (typeof value === 'boolean') {
-//           return value.toString();
-//         } else if (typeof value === 'number') {
-//           return Boolean(value).toString();
-//         }
-  
-//         return value;
-//       }
-  
-//       if (type === 'number' && format === 'percent') {
-//         return [parseFloat(value).toFixed(2), '%'].join('');
-//       }
-  
-//       return value.toString();
-//     }
-  
-//     function format(row) {
-//       return Object.fromEntries(
-//         Object.entries(row).map(([dataIndex, value]) => {
-//           return [dataIndex, formatValue(value, typeByIndex[dataIndex])];
-//         })
-//       );
-//     }
-  
-//     return data.map(format);
-//   };
-
-// const TableRenderer = ({ resultSet, pivotConfig }) => {
-//     const [tableColumns, dataSource] = useDeepCompareMemo(() => {
-//         const columns = resultSet.tableColumns(pivotConfig);
-//         return [
-//         columns,
-//         formatTableData(columns, resultSet.tablePivot(pivotConfig)),
-//         ];
-//     }, [resultSet, pivotConfig]);
-//     return (
-//         <Table pagination={false} columns={tableColumns} dataSource={dataSource} />
-//     );
-//     };
-
-// class RawDataTable extends Component {
-//     constructor() {
-//         super ()
-
-//         this.colors = ['#FF6492', '#141446', '#7A77FF'];
-
-//         this.cubejsApi = cubejs(
-//             'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NDAyMjYyNjEsImV4cCI6MTY0MDMxMjY2MX0.MyjDXXR7xRqL39X3m_tz4Gxo6H5IayVEWpz32W_nG0U',
-//             { apiUrl: 'http://localhost:4000/cubejs-api/v1' }
-//             );
-
-//         this.renderChart = this.renderChart.bind(this);
-//     }
-
-    
-
-//     renderChart = ({ resultSet, error, pivotConfig }) => {
-//         if (error) {
-//           return <div>{error.toString()}</div>;
-//         }
-      
-//         if (!resultSet) {
-//           return <Spin />;
-//         }
-      
-//         return <TableRenderer resultSet={resultSet} pivotConfig={pivotConfig} />;
-//       };
-    
-
-//     render() {
-//         return (
-//             <QueryRenderer
-//               query={{
-//           "measures": [],
-//           "timeDimensions": [],
-//           "order": [
-//             [
-//               "Corecrimedata.updatedOn",
-//               "desc"
-//             ],
-//             [
-//               "Commareas.communityDesc",
-//               "asc"
-//             ]
-//           ],
-//           "dimensions": [
-//             "Corecrimedata.caseNumber",
-//             "Corecrimedata.date",
-//             "Corecrimedata.primaryType",
-//             "Corecrimedata.description",
-//             "Corecrimedata.locationDescription",
-//             "Corecrimedata.arrest",
-//             "Corecrimedata.district",
-//             "Corecrimedata.xCoordinate",
-//             "Corecrimedata.yCoordinate",
-//             "Corecrimedata.updatedOn",
-//             "Corecrimedata.latitude",
-//             "Corecrimedata.longitude",
-//             "Commareas.communityDesc"
-//           ],
-//           "filters": [
-//             {
-//               "member": "Commareas.communityDesc",
-//               "operator": "equals",
-//               "values": [
-//                 "AUSTIN"
-//               ]
-//             }
-//           ],
-//           "limit": 100
-//         }}
-//               cubejsApi={this.cubejsApi}
-//               resetResultSetOnChange={false}
-//               render={(props) => this.renderChart({
-//                 ...props,
-//                 chartType: 'table',
-//                 pivotConfig: {
-//           "x": [
-//             "Corecrimedata.caseNumber",
-//             "Corecrimedata.date",
-//             "Corecrimedata.primaryType",
-//             "Corecrimedata.description",
-//             "Corecrimedata.locationDescription",
-//             "Corecrimedata.arrest",
-//             "Corecrimedata.district",
-//             "Corecrimedata.xCoordinate",
-//             "Corecrimedata.yCoordinate",
-//             "Corecrimedata.updatedOn",
-//             "Corecrimedata.latitude",
-//             "Corecrimedata.longitude",
-//             "Commareas.communityDesc"
-//           ],
-//           "y": [],
-//           "fillMissingDates": true,
-//           "joinDateRange": false,
-//           "limit": 100
-//         }
-//               })}
-//             />
-//           );
-//     }
-// }
-
-// export default RawDataTable
