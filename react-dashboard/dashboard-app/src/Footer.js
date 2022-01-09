ddbb99292cae2f24e6dca332c2dac3f1c8f830a9
Aaron <chanman2841@gmail.com>
2022-01-08 18:34:20 -0600

    more progress on UI design

--- react-dashboard/dashboard-app/src/Footer.js
+++ react-dashboard/dashboard-app/src/Footer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import './styles/faked-footer.css'
+import './styles/faked-footer.scss'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -17,7 +17,12 @@ class Footer extends Component {
                                 <h3>About</h3>
                                 <hr></hr>
                                 <p>This dashboard is updated every _______ at ____ CST.</p>
-                                <p>Data is gathered from the <a href='https://data.cityofchicago.org/Public-Safety/Crimes-2001-to-present-Dashboard/5cd6-ry5g'>Chicago Data Portal</a>.</p>
+                                <div class="link-cont">
+                                    <div class="link-wrapper">
+                                        Data is gathered from the <a class="link hover-2" href='https://data.cityofchicago.org/Public-Safety/Crimes-2001-to-present-Dashboard/5cd6-ry5g' target="_blank" rel="noopener noreferrer">Chicago Data Portal</a>
+                                    </div>
+
+                                </div>
                                 <p>©2021 Chan</p>
                             </div>
                         </Col>
@@ -25,12 +30,17 @@ class Footer extends Component {
                             <div className='footer-text'>
                                 <h3>Links</h3>
                                 <hr></hr>
-                                <p><a href='https://github.com/Achan40'>GitHub</a></p>
-                                <p><a href='https://www.linkedin.com/in/aaron-chan-30393115a/'>LinkedIn</a></p>
+                                <div class="link-cont">
+                                    <div class="link-wrapper">
+                                        <a class="link hover-2" href='https://github.com/Achan40' target="_blank" rel="noopener noreferrer">GitHub</a>
+                                    </div>
+                                    <div clas="link-wrapper">
+                                        <a class="link hover-2" href='https://github.com/Achan40' target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                                    </div>
+                                </div>
                             </div>
                         </Col>
                     </Row>
-                    
                 </Container>
             </div>
         )
